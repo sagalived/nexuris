ea765d6826d2e865023a57c133f19a6d69639986
Junior Morais <79609439+sagalived@users.noreply.github.com>
2026-04-14 00:13:53 -0300

    render
    ae

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,8 +1,17 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChanged, signInWithEmailAndPassword, createUserWithEmailAndPassword } from 'firebase/auth';
 import { getFirestore, doc, getDoc, setDoc, collection, query, where, orderBy, limit, onSnapshot, addDoc, updateDoc, deleteDoc, getDocFromServer } from 'firebase/firestore';
-// @ts-ignore
-import firebaseConfig from '../../firebase-applet-config.json';
+
+const firebaseConfig = {
+  apiKey: process.env.VITE_FIREBASE_API_KEY,
+  authDomain: process.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.VITE_FIREBASE_APP_ID,
+  measurementId: process.env.VITE_FIREBASE_MEASUREMENT_ID,
+  firestoreDatabaseId: process.env.VITE_FIREBASE_DATABASE_ID,
+};
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
